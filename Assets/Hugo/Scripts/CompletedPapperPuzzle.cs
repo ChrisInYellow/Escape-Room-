0f24c5054f7a01f2d8d94f1ecdaa9f9093c77229
Hugo Cremer <slasken49@gmail.com>
2018-01-29 15:30:16 +0100

    PapperPuzzle
    Publishing the papper puzzle needs testing from another human being.

---
+++ Assets/Hugo/Scripts/CompletedPapperPuzzle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompletedPapperPuzzle : MonoBehaviour
+{
+    [HideInInspector]
+    public float numberOfPlacedPappers = 0;
+    public GameObject puzzleSolved;
+
+    public void IncreasePlacedPappers ()
+    {
+        numberOfPlacedPappers += 1;
+        if (numberOfPlacedPappers == 4)
+        {
+            Instantiate(puzzleSolved, transform.position, Quaternion.identity);
+        }
+    }
+}
