4f5fd21d4341c1c7b4bfae142152332912802b8a
eltonr98 <31646723+eltonr98@users.noreply.github.com>
2018-01-29 14:53:17 +0100

    Completed scripts and stuff yo for the lock
    yup

---
+++ Assets/Scripts/ArrowNumberWheel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowNumberWheel : MonoBehaviour {
+
+    [SerializeField]
+    public enum Direction {None, Up, Down };
+    public Direction currentDirection = Direction.None;
+
+    public GameObject spinner;
+    Animator anim;
+
+    public void Start()
+    {
+        anim = spinner.gameObject.GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(currentDirection == Direction.Up)
+        {
+            anim.SetInteger("numbers", (anim.GetInteger("numbers")+1)%10);
+        }
+
+        else if(currentDirection == Direction.Down)
+        {
+            anim.SetInteger("numbers", (anim.GetInteger("numbers") - 1) % 10);
+        }      
+    }
+}
