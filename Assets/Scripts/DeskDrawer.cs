f95233d8865d6e7405ae1f248c855674128413e1
trustred <trust870@hotmail.com>
2018-01-30 09:06:38 +0100

    Commit

---
+++ Assets/Scripts/DeskDrawer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeskDrawer : MonoBehaviour {
+
+    public bool isOpen;
+    private Animator drawerAnim;
+
+    private void Start()
+    {
+        drawerAnim = transform.GetChild(0).GetComponent<Animator>();
+    }
+
+    public void OpenDrawer()
+    {
+        if (isOpen)
+        {
+            drawerAnim.SetBool("Open", true);
+        }
+    }
+
+    public void Update()
+    {
+        if (isOpen)
+        {
+            drawerAnim.SetBool("Open", true);
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/FlashLight.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLight : MonoBehaviour {
+
+    public bool isOn;
+    private GameObject spotLight;
+
+    private void Start()
+    {
+        spotLight = transform.GetChild(0).gameObject;
+    }
+
+    public void Activate()
+    {
+        if (isOn)
+        {
+            isOn = false;
+            spotLight.SetActive(false);
+        }
+        else
+        {
+            isOn = true;
+            spotLight.SetActive(true);
+        }
+    }
+
+}
