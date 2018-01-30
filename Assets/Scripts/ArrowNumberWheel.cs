bf2445728efb304ab5e26a7881e5270b1d11aa1e
eltonr98 <31646723+eltonr98@users.noreply.github.com>
2018-01-30 10:19:02 +0100

    Fixed NumberSpinner Should Work Correctly now

--- Assets/Scripts/ArrowNumberWheel.cs
+++ Assets/Scripts/ArrowNumberWheel.cs
@@ -6,7 +6,13 @@ public class ArrowNumberWheel : MonoBehaviour {
 
     [SerializeField]
     public enum Direction {None, Up, Down };
+    public float cooldownTimer; 
     public Direction currentDirection = Direction.None;
+    private bool CD;
+
+    [SerializeField]
+    public enum SpinnerIdentifier {None,One,Two,Three,Four};
+    public SpinnerIdentifier spinnerNumber = SpinnerIdentifier.None;
 
     public GameObject spinner;
     Animator anim;
@@ -18,6 +24,9 @@ public class ArrowNumberWheel : MonoBehaviour {
 
     private void OnTriggerEnter(Collider other)
     {
+        if (CD)  
+            return;
+        StartCoroutine(Cooldown(cooldownTimer));
         if(currentDirection == Direction.Up)
         {
             if(!anim.GetCurrentAnimatorStateInfo(0).IsName("Numbers"))
@@ -27,7 +36,17 @@ public class ArrowNumberWheel : MonoBehaviour {
         else if(currentDirection == Direction.Down)
         {
             if (!anim.GetCurrentAnimatorStateInfo(0).IsName("Numbers"))
+                if(anim.GetInteger("Numbers") < 0)
+                {
+                    anim.SetInteger("Numbers", 10);
+                }
                 anim.SetInteger("Numbers", (anim.GetInteger("Numbers") - 1) % 10);
         }      
+    }
+    IEnumerator Cooldown(float time)
+    {
+        CD = true;
+        yield return new WaitForSeconds(time);
+        CD = false;
     }
 }
