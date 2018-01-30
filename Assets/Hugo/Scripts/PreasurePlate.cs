6bffb746bc9a66c18ffe45b597df775e20925539
Hugo Cremer <slasken49@gmail.com>
2018-01-30 11:28:26 +0100

    Preasure plate and suffs

---
+++ Assets/Hugo/Scripts/PreasurePlate.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreasurePlate : MonoBehaviour
+{
+    public float maxWeight;
+    public float minWeight;
+
+    public float currentWeight;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<ItemProperties>() == true)
+        {
+            currentWeight += other.gameObject.GetComponent<ItemProperties>().weight;
+            CheckWeight();
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.GetComponent<ItemProperties>() == true)
+        {
+            currentWeight -= other.gameObject.GetComponent<ItemProperties>().weight;
+            CheckWeight();
+        }
+    }
+
+    public void CheckWeight ()
+    {
+        if (currentWeight <= maxWeight && currentWeight >= minWeight)
+        {
+            Debug.Log("Enough weight");
+        }
+    }
+}
