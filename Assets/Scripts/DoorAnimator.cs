e196f5e33ff2c463cb057b2617dcb0f929a1a327
ChrisInYellow <31647524+ChrisInYellow@users.noreply.github.com>
2018-01-30 17:31:57 +0100

    DoorAnimation
    Added dooranim.

---
+++ Assets/Scripts/DoorAnimator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAnimator : MonoBehaviour {
+
+    public Animator anim;
+
+    public void OpenDoor()
+    {
+        anim.SetTrigger("DoorOpens"); 
+    }
+
+
+}
