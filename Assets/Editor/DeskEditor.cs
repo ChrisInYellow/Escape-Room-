4514895e5eaf196a311813eec210687583291e95
trustred <trust870@hotmail.com>
2018-01-31 09:14:09 +0100

    commit

---
+++ Assets/Editor/DeskEditor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Desk))]
+public class DeskEditor : Editor {
+
+	public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        Desk desk = (Desk)target;
+        if (GUILayout.Button("Open"))
+        {
+            desk.Open();
+        }
+    }
+}
